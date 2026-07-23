3607db14259bc26ac9711bb7ad8abe43bd9f5e5b
asgarali9 <113428581+asgarali9@users.noreply.github.com>
2026-07-24 01:44:56 +0300

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Welcome to Java Session, Multi-Cloud DevOps Journey of Asgar Ali Mohammad");
-        System.out.println("Welcome to Java Session, Multi-Cloud DevOps Journey with Vikas sir");
+     
     }
 }
