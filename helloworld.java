ec38c1693fb394c727ecb5cbd5b7da5fff9e5baa
asgarali9 <asgarali.na@gmail.com>
2026-07-24 02:05:15 +0300

    changes in local jar setup

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,6 @@ public class helloworld {
     public static void main(String[] args) {
         System.out.println("Welcome to Java Session, Multi-Cloud DevOps Journey of Asgar Ali");
         System.out.println("Welcome to Java Session, jenkin integration with GitHub, Multi-Cloud DevOps Journey of Asgar Ali");
+        System.out.println("Welcome to Java Session, jenkin jar bin file is added in local system path, ");
     }
 }
