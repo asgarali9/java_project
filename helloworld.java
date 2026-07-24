1d671d25e52f0e677ece7524023126cf0b793ab6
asgarali9 <asgarali.na@gmail.com>
2026-07-24 14:02:31 +0300

    5th day of devops class moven

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,6 @@ public class helloworld {
         System.out.println("Welcome to Java Session, Multi-Cloud DevOps Journey of Asgar Ali");
         System.out.println("Welcome to Java Session, jenkin integration with GitHub, Multi-Cloud DevOps Journey of Asgar Ali");
         System.out.println("Welcome to Java Session, jenkin jar bin file is added in local system path, ");
-        System.out.println("maven class ");
+        System.out.println("DevOps class 5th day for maven class ");
     }
 }
